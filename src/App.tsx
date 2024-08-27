e6fdf69e78c1dbb052b5ec2b9ef60997386e7e14
uvenven <fadilefd1102@gmail.com>
2024-08-27 10:59:59 +0700

    feat: Create UI for ResepSaji application - Create components for category and food cards

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Browse from './pages/Browse';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Browse />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
